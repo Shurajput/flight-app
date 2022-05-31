936148a6293397b7b5eb825ebd77f262d04d5f5a
Shurajput <105844014+Shurajput@users.noreply.github.com>
2022-05-31 22:41:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function setvisibility(s4)
+ {
+                  alert("Tikets Are Availables");
+                  document.getElementById(s4).style.display='inline' 
+
+}                  
+                
+function subb2() {
+      alert("Are You Want To Confirm Ticket");
+      
+      alert("Your Ticket Is Successfully Booked");
+     
+}
